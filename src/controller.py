5828ea93d55ef1caa74421de3e639fc0283ee9d8
BenYKB <benjaminykb@gmail.com>
2020-11-17 21:01:02 -0800

    update sim time flag

--- src/controller.py
+++ src/controller.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import Image
 #import pyqrcode
 #import random
 from std_msgs.msg import String
+from std_msgs.msg import Time
 import rospy
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
@@ -26,10 +27,11 @@ START_UP_WAIT_TIME = 3
 
 class controller():
     def __init__(self):
-        rospy.init_node('controller', anonymous=True)
+        self.initialized = False
 
+        rospy.init_node('controller', anonymous=True)
         self._image_sub = rospy.Subscriber('/R1/pi_camera/image_raw', Image, callback=self._image_callback, queue_size=1)
-        #self._clock_sub = rospy.Subscriber('/clock', rospy.Time, callback=self._time_callback, queue_size=1)
+        self._clock_sub = rospy.Subscriber('/clock', Time, callback=self._time_callback, queue_size=1)
         
         self._twist_pub = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=1)
         self._image_pub = rospy.Publisher('/R1/debug_image', Image, queue_size=1)
@@ -39,10 +41,13 @@ class controller():
         self.image_count = 0
         self.state = States.STARTING
         self.seconds = 0
-        rospy.Timer(rospy.Duration(1),self._on_timer)
+        rospy.Timer(rospy.Duration(1), self._on_timer)
 
 
     def _on_timer(self, time):
+        if not self.initialized:
+            return
+
         self.seconds += 1
         
         if self.seconds == 1:
@@ -58,9 +63,9 @@ class controller():
         if self.state == States.STARTING and self.seconds >= START_UP_WAIT_TIME:
             self.state = States.FOLLOWING
 
-    # def _time_callback(self, time):
-    #     pass
-        #raise Exception(time) #or something
+    def _time_callback(self, time):
+        if not self.initialized and rospy.get_time() > 0:
+            self.initialized = True
 
     def start_timer(self):
         self._licence_pub.publish(str('team1,xxxx,0,AA00'))
@@ -70,6 +75,9 @@ class controller():
 
 
     def _image_callback(self, image):
+        if not self.initialized:
+            return
+        
         try:
             cv_image = self._bridge.imgmsg_to_cv2(image, "bgr8")
         except CvBridgeError as e:
