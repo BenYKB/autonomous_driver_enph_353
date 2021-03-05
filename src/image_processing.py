71495c85673dd54f4471e1c93f1a73b0a476e4ba
Davis Johnson <54873766+johnsonadavis@users.noreply.github.com>
2021-03-05 11:02:55 -0800

    Update image_processing.py
    Removed unnecessary commented lines

--- src/image_processing.py
+++ src/image_processing.py
@@ -7,9 +7,6 @@ import numpy as np
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 import os
-#import pyqrcode
-#import random
-#import string
 import rospy
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
@@ -26,12 +23,12 @@ from tensorflow.python.keras.models import load_model
 
 import tensorflow as tf
 
-
-
-
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 char_lookup = dict(zip([a for a in alphabet],[i for i in range(len(alphabet))]))
 char_inverse_lookup = dict(map(reversed, char_lookup.items()))
+
+HIGH_CONFIDENCE = 0.7
+LOW_CONFIDENCE = 0.5
 
 class license_plate_detection():
     def __init__(self):
@@ -42,7 +39,6 @@ class license_plate_detection():
 
         self.model = load_model(os.path.dirname(os.path.abspath(__file__)) +'/model2')
         self.model._make_predict_function()
-        #self.model.summary()
 
         self._bridge = CvBridge()
         self._image_sub = rospy.Subscriber('/R1/pi_camera/image_raw', Image, callback=self._image_callback, queue_size=1)
@@ -66,25 +62,16 @@ class license_plate_detection():
     def _get_loc_plate(self, letters):
         output= ''
         for letter in letters:
-            #letter = cv2.imread('/home/fizzer/ros_ws/src/autonomous_driver_enph_353/src/test _images/plate_BF17.png', cv2.IMREAD_GRAYSCALE)[5:,:]
-            #letter_reshape = letter.reshape(letter.shape[0], letter.shape[1], 1)
-            #letter = cv2.cvtColor(letter, cv2.COLOR_BGR2RGB)
             letter = letter*1./255
             img_aug = np.expand_dims(letter, axis=0)
-            #print(img_aug.dtype)
-            #print(img_aug.shape)
             with self.graph.as_default():
                 tf.keras.backend.set_session(self.sess)
                 y_predict = self.model.predict(img_aug)[0]
-                #y_predict = self.CNN.predict(letter)
                 prediction = char_inverse_lookup[np.argmax(y_predict)]
                 output = output + prediction
-                #print(prediction)
-        #print(output)
         if is_valid(output):
             print('valid output loc plate')
             print(output)
-            #print(output)
             return output[1], output[2:]
         else:
             return None, None
@@ -97,9 +84,7 @@ class license_plate_detection():
         print(confidence)
         output = ','.join([loc, plate[0], plate[1], plate[2], plate[3], str(confidence)])
         print("sending output" + str(output))
-        #print('6,B,A,0,2,1000.2')
         self._license_pub.publish(String(output))
-        #self._license_pub.publish(output)
 
 
 def deskew(img, pts1, pts2, shape):
@@ -144,7 +129,6 @@ def find_order_pts(points):
 
 def parse_image(img):
     hsv_frame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #cv2.imshow('hsv', hsv_frame)
     shape = (1280, 720)
 
     normal_lower_hsv = np.array([0,0,86])
@@ -164,24 +148,12 @@ def parse_image(img):
     img = cv2.resize(img, shape)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    #gray = cv2.bilateralFilter(img_mask, 25, 100, 100) #300,300
     gray = cv2.GaussianBlur(img_mask,(21,21),0)
     t, gray = cv2.threshold(gray, 127,255,cv2.THRESH_BINARY)
 
-    #print(gray.shape)
-    #cv2.imshow('mask', gray)
-
     edged = cv2.Canny(gray, 30, 200)
-    #cv2.imshow('edges', edged )
-
 
     im2, contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #print(contours)
-
-    #cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
-
-    #cv2.imshow('m',img)
-    #cv2.waitKey(0)
 
     contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
     screenCnt1 = None
@@ -192,7 +164,6 @@ def parse_image(img):
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.1 * peri, True)
         cv2.drawContours(img, [approx], -1, (255, 255, 255), 3)
-        #print(len(approx))
         if len(approx) == 4:
             if screenCnt1 is None:
                 screenCnt1 = approx
@@ -203,20 +174,18 @@ def parse_image(img):
         return None, None
     else:
         screenCnt1 = find_order_pts(screenCnt1)
-        #cv2.imshow('edges', edged )
-        #cv2.imshow('mask', gray)
 
         if 120 > screenCnt1[1,0]:
             if 50 > screenCnt1[1,0]:
-                confidence = confidence * 0.5
+                confidence = confidence * LOW_CONFIDENCE
             else:
-                confidence = confidence * 0.7
+                confidence = confidence * HIGH_CONFIDENCE
 
         if screenCnt1[2,0] > 1280-120:
             if screenCnt1[2,0] > 1280-50:
-                confidence = confidence * 0.5
+                confidence = confidence * LOW_CONFIDENCE
             else:
-                confidence = confidence * 0.7
+                confidence = confidence * HIGH_CONFIDENCE
 
         detected = deskew(img, screenCnt1, [[0,0],[0,1800],[600,1800],[600,0]], (600,1800))
 
@@ -227,19 +196,17 @@ def parse_image(img):
         location_letters = [location[650:1100,0:300],location[650:1100,300:]]
 
         letters = []
-        model_shape = ( 100, 150)
+        model_shape = (100, 150)
 
         try:
             for j in range(2):
                 letter = location_letters[j]
                 letter = cv2.resize(letter, model_shape)
-                #cv2.imshow('location'+str(j), letter)
                 letters.append(letter)
 
             for j in range(4):
                 letter = plate_letters[j]
                 letter = cv2.resize(letter, model_shape)
-                #cv2.imshow('plate' + str(j), letter)
                 letters.append(letter)
         except Exception as e:
             print(e)
@@ -251,16 +218,6 @@ def is_valid(string):
     p = re.compile("^[P][0-9][A-Z][A-Z][0-9][0-9]$")
 
     return True if p.match(string) else False
-
-    # #return True
-    # if not string[0]=='P':
-    #     return False
-    # elif not string[2:4].isalpha():
-    #     return False
-    # elif not string[1].isnumeric() or not string[4:].isnumeric():
-    #     return False
-    # else:
-    #     return True
 
 
 if __name__ == "__main__":
